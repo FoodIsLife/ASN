e61183afdc6e2ff329b9711ef5cb9d0805acaaa6
pageflourin <pageflourin@gmail.com>
2018-06-07 20:53:05 +0800

    added model

---
+++ server/boot/events-route.js
@@ -0,0 +1,30 @@
+var dsConfig = require('../datasources.json');
+var path = require('path');
+var passport =  require('passport');
+
+module.exports = function(app) {
+    var router = app.loopback.Router();
+    var Events = app.models.Events;
+
+    //endpoints
+
+    //Create events
+
+    //get all events
+
+    //get event by id
+
+    //get event where filter is date range
+
+    //get event where filter is exact date
+
+    //get event by name
+
+
+    //modify event details
+
+
+    
+    
+
+};
---
+++ server/boot/gigs-route.js
@@ -0,0 +1,32 @@
+var dsConfig = require('../datasources.json');
+var path = require('path');
+var passport =  require('passport');
+
+module.exports = function(app) {
+    var router = app.loopback.Router();
+    var Gigs = app.models.Gigs;
+
+    //endpoints
+
+    //Create gig
+
+    //get all gigs of artist
+
+    //get gig by id
+
+    //get gig where filter is date range
+
+    //get gig where filter is exact date
+
+    //get gig by name
+
+
+    //modify gig details
+
+    //delete gig
+
+
+    
+    
+
+};
