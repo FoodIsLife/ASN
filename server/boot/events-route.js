72e0136fb5d9480e0005d8e622930d68974d6375
pageflourin <pageflourin@gmail.com>
2018-06-08 16:41:43 +0800

    rest of the models

--- server/boot/events-route.js
+++ server/boot/events-route.js
@@ -25,6 +25,6 @@ module.exports = function(app) {
 
 
     
-    
+    app.use(router);
 
 };
--- server/boot/gigs-route.js
+++ server/boot/gigs-route.js
@@ -26,7 +26,7 @@ module.exports = function(app) {
     //delete gig
 
 
-    
+    app.use(router);
     
 
 };
